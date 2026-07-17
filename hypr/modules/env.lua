3557973265e708736390c3dc5d390583160c64a0
Alexei Santillan <falexei.santillan@gmail.com>
2026-07-17 02:59:53 -0600

    fix: use $HOME instead of hardcoded path in HYPRSHOT_DIR

--- hypr/modules/env.lua
+++ hypr/modules/env.lua
@@ -1,11 +1,10 @@
 -------------------------------
 ---- ENVIRONMENT VARIABLES ----
 -------------------------------
-
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
--- Carpeta donde hyprshot guarda las capturas de pantalla
-hl.env("HYPRSHOT_DIR", "/home/alexei/Pictures/Screenshots")
+-- Screenshot folder 
+hl.env("HYPRSHOT_DIR", os.getenv("HOME") .. "/Pictures/Screenshots")
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
@@ -21,7 +20,7 @@ hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_TYPE", "wayland")
 hl.env("XDG_SESSION_DESKTOP", "Hyprland")
 
--- QT 
+-- QT
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
