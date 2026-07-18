138e90905b35620bcb402cb5fc5ec6ed39f0b451
Alexei Santillan <falexei.santillan@gmail.com>
2026-07-18 14:09:13 -0600

    feat: integrate floating controls for volume and network modules

--- hypr/modules/windowrules.lua
+++ hypr/modules/windowrules.lua
@@ -1,4 +1,3 @@
-
 --------------------------------
 ---- WINDOWS AND WORKSPACES ----
 --------------------------------
@@ -6,20 +5,25 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
 -- and https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
 
--- Example window rules that are useful
-
+-- Ignore maximize requests from all applications.
+-- This helps prevent applications from unexpectedly changing
+-- the layout managed by Hyprland.
 local suppressMaximizeRule = hl.window_rule({
-    -- Ignore maximize requests from all apps. You'll probably like this.
-    name  = "suppress-maximize-events",
-    match = { class = ".*" },
+    name = "suppress-maximize-events",
+    match = {
+        class = ".*",
+    },
 
     suppress_event = "maximize",
 })
+
+-- Uncomment this to disable the rule:
 -- suppressMaximizeRule:set_enabled(false)
 
+
+-- Fix some dragging issues with XWayland windows.
 hl.window_rule({
-    -- Fix some dragging issues with XWayland
-    name  = "fix-xwayland-drags",
+    name = "fix-xwayland-drags",
     match = {
         class      = "^$",
         title      = "^$",
@@ -32,19 +36,45 @@ hl.window_rule({
     no_focus = true,
 })
 
--- Layer rules also return a handle.
--- local overlayLayerRule = hl.layer_rule({
---     name  = "no-anim-overlay",
---     match = { namespace = "^my-overlay$" },
---     no_anim = true,
--- })
--- overlayLayerRule:set_enabled(false)
 
--- Hyprland-run windowrule
+-- Move the Hyprland command runner to the bottom-right corner.
 hl.window_rule({
-    name  = "move-hyprland-run",
-    match = { class = "hyprland-run" },
+    name = "move-hyprland-run",
+    match = {
+        class = "hyprland-run",
+    },
 
     move  = "20 monitor_h-120",
     float = true,
+})
+
+
+-- Pavucontrol floating window.
+-- org.pulseaudio.pavucontrol
+hl.window_rule({
+    name = "float-pavucontrol",
+    match = {
+        class = "org.pulseaudio.pavucontrol",
+    },
+
+    float = true,
+    center = true,
+})
+
+
+-- Network management terminal floating window.
+-- The terminal must be started with the custom class:
+--
+-- alacritty --class nmtui-scratchpad -e nmtui
+--
+-- This allows Hyprland to identify this specific terminal
+-- independently from other Alacritty windows.
+hl.window_rule({
+    name = "float-nmtui",
+    match = {
+        class = "^nmtui-scratchpad$",
+    },
+
+    float = true,
+    center = true,
 })
